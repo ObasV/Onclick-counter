49c2c1a0f32a81e6835038d09b6bdba4b62b6235
ObasV <105185909+ObasV@users.noreply.github.com>
2022-09-05 13:31:42 +0100

    Create csswork.css

---
+++ csswork.css
@@ -0,0 +1,24 @@
+let countEl = document.getElementById("count")
+console.log(countEl)
+
+
+let count=0
+function increament(){
+    count=count +1  
+    console.log(count)
+    countEl.innerText=count
+}
+
+
+
+
+let display = document.getElementById("show")
+console.log(display)
+
+function saveincr(){
+    let oldCount = count + ", "
+    display.innerText +=  oldCount
+    countEl.textContent=0
+    count=0
+    
+}
